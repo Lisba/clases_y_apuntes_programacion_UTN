fb5e2de02290633bac4e218c49bfbe6635a935ab
Lisbaldy <lisba7474@gmail.com>
2019-09-14 18:28:04 -0300

    Funciones agregadas a mi libreria

--- clase07/funciones_ordenamiento_buble/main.c
+++ clase07/funciones_ordenamiento_buble/main.c
@@ -1,60 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define T 3
+#define T 8
+#define NOTAS 2
 
-void mostrarUnAlumno(int, int, char, int,int,int, float);
-void mostrarAlumnos(int[], int[], char[], int[],int[],int[], float[], int);
-void ordenarAlumnos(int[], int[], char[], int[],int[],int[], float[], int, char[], char);
+void pedirAlumno(int[], int[], char[], int[], int[], float[], int);
+void mostrarUnAlumno(int, int, char, int, int, float);
+void mostrarAlumnos(int[], int[], char[], int[], int[], float[], int);
+void ordenarAlumnos(int[], int[], char[], int[], int[], float[], int);
 
 int main()
 {
-    int legajo[T]= {101,102,105};
-    int edad[T]= {15,16,12};
-    char sexo[T]= {'m','f','f'};
-    int nota1[T]= {10,5,9};
-    int nota2[T]= {7,4,6};
-    int nota3[T]= {3,3,8};
-    float promedio[T]= {6.66, 4.00, 7.66};
+    int legajo[T]= {101, 102, 105, 100, 103, 104};
+    int edad[T]= {27, 18, 19, 20, 25, 26};
+    char sexo[T]= {'m', 'f', 'f', 'm', 'f', 'm'};
+    int nota1[T]= {10, 5, 9, 8, 7, 6};
+    int nota2[T]= {7, 4, 6, 8, 9, 7};
+    float promedio[T] = {8.5, 4.5, 7.5, 8, 8, 6.5};
 
-    mostrarAlumnos(legajo[T], edad[T], sexo[T], nota1[T], nota2[T], nota3[T], promedio[T], T)
+    mostrarAlumnos(legajo, edad, sexo, nota1, nota2, promedio, T);
+    ordenarAlumnos(legajo, edad, sexo, nota1, nota2, promedio, T);
+    mostrarAlumnos(legajo, edad, sexo, nota1, nota2, promedio, T);
+    pedirAlumno(legajo, edad, sexo, nota1, nota2, promedio, 2);
+    ordenarAlumnos(legajo, edad, sexo, nota1, nota2, promedio, T);
+    mostrarAlumnos(legajo, edad, sexo, nota1, nota2, promedio, T);
 
     return 0;
 }
 
-void mostrarUnAlumno(int legajo, int edad, char sexo, int nota1,int nota2,int nota3, float promedio)
+void pedirAlumno(int legajo[], int edad[], char sexo[], int nota1[], int nota2[], float promedio[], int tam)
 {
-     printf("%d\t%d\t%c\t%d\t%d\t%d\t%f\n",
-                   legajo,
-                   edad,
-                   sexo,
-                   nota1,
-                   nota2,
-                   nota3,
-                   promedio);
-}
 
-void mostrarAlumnos(int legajo[], int edad[], char sexo[], int nota1[],int nota2[],int nota3[], float promedio[], int tam)
-{
-    int i;
     for(int i=0; i<tam; i++)
     {
-        mostrarUnAlumno(legajo[i], edad[i], sexo[i], nota1[i], nota2[i], nota3[i], promedio[i]);
+        printf("Ingrese el legajo: ");
+        scanf("%d", &legajo[i]);
+
+        printf("Ingrese la edad: ");
+        scanf("%d", &edad[i]);
+
+        printf("Ingrese el sexo: ");
+        fflush(stdin);
+        scanf("%c", &sexo[i]);
+
+        printf("Ingrese la nota1: ");
+        scanf("%d", &nota1[i]);
+
+        printf("Ingrese la nota2: ");
+        scanf("%d", &nota2[i]);
+
+        promedio[i] = (float) (nota1[i] + nota2[i]) / NOTAS;
+    }
+}
+
+void mostrarUnAlumno(int legajo, int edad, char sexo, int nota1, int nota2, float promedio)
+{
+     printf("%d\t%d\t%c\t%d\t%d\t%.3f\n", legajo, edad, sexo, nota1, nota2, promedio);
+}
+
+void mostrarAlumnos(int legajo[], int edad[], char sexo[], int nota1[], int nota2[], float promedio[], int tam)
+{
+    printf("\nleg:\tedad:\tsexo:\tn1:\tn2:\tpromedio:\n\n");
+    for(int i=0; i<tam; i++)
+    {
+        mostrarUnAlumno(legajo[i], edad[i], sexo[i], nota1[i], nota2[i], promedio[i]);
     }
 
 }
 
-void ordenarAlumnos(int legajo[], int edad[], char sexo[], int nota1[],int nota2[],int nota3[], float promedio[], int tam, char criterio[], char ascDes)
+void ordenarAlumnos(int legajo[], int edad[], char sexo[], int nota1[], int nota2[], float promedio[], int tam)
 {
+
     int auxInt;
     char auxChar;
     float auxFloat;
+    int swap = 0;
 
     for(int i=0; i<tam-1; i++)
     {
+
         for(int j=i+1; j<tam; j++)
         {
+
             if(sexo[i] > sexo[j])
             {
+                swap = 1;
+
+            } else if (sexo[i] == sexo[j] && legajo[i] > legajo[j])
+            {
+                swap = 1;
+            }
+
+            if (swap == 1)
+            {
+
                 auxInt = legajo[i];
                 legajo[i] = legajo[j];
                 legajo[j] = auxInt;
@@ -63,9 +101,9 @@ void ordenarAlumnos(int legajo[], int edad[], char sexo[], int nota1[],int nota2
                 edad[i] = edad[j];
                 edad[j] = auxInt;
 
-                auxInt = sexo[i];
+                auxChar = sexo[i];
                 sexo[i] = sexo[j];
-                sexo[j] = auxInt;
+                sexo[j] = auxChar;
 
                 auxInt = nota1[i];
                 nota1[i] = nota1[j];
@@ -75,43 +113,11 @@ void ordenarAlumnos(int legajo[], int edad[], char sexo[], int nota1[],int nota2
                 nota2[i] = nota2[j];
                 nota2[j] = auxInt;
 
-                auxInt = nota3[i];
-                nota3[i] = nota3[j];
-                nota3[j] = auxInt;
-
                 auxFloat = promedio[i];
                 promedio[i] = promedio[j];
                 promedio[j] = auxFloat;
 
-            }else if(sexo[i] == sexo[j] && legajo[i] > legajo[j])
-            {
-                                auxInt = legajo[i];
-                legajo[i] = legajo[j];
-                legajo[j] = auxInt;
-
-                auxInt = edad[i];
-                edad[i] = edad[j];
-                edad[j] = auxInt;
-
-                auxInt = sexo[i];
-                sexo[i] = sexo[j];
-                sexo[j] = auxInt;
-
-                auxInt = nota1[i];
-                nota1[i] = nota1[j];
-                nota1[j] = auxInt;
-
-                auxInt = nota2[i];
-                nota2[i] = nota2[j];
-                nota2[j] = auxInt;
-
-                auxInt = nota3[i];
-                nota3[i] = nota3[j];
-                nota3[j] = auxInt;
-
-                auxFloat = promedio[i];
-                promedio[i] = promedio[j];
-                promedio[j] = auxFloat;
+                swap = 0;
             }
         }
     }
--- libreria_utn/lisba_utn.c
+++ libreria_utn/lisba_utn.c
@@ -1,3 +1,216 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define "lisba_utn.h"
+
+/** \brief Solicita un número al usuario y devuelve dicho número.
+ *
+ * \param void No recibe parametros.
+ * \return float El número ingresado por el usuario.
+ *
+ */
+float getFloat(char message[])
+{
+    float number;
+    int validation;
+
+    do
+    {
+        fflush(stdin); // Limpieza del buffer de entrada ya que en caso de ingresar un caracter no válido quedará enter en memoria e iterará infinitamente.
+        printf("\n======================\n");
+        printf("%s", message);
+        validation = scanf("%f", &number); //Validación para números, en caso de ingresar un caracter distinto a un número la variable validation valdrá 0.
+
+        if(!validation) // Si la variable validation vale 0 se muestra el mensaje de error.
+        {
+            printf("\n%cERROR! DEBE INGRESAR UN N%cMERO.\n", 173, 233);
+        }
+
+    }while(!validation); // Si la variable validation es 0 se repite el ciclo hasta que validation sea distinta de 0, es decir, hasta ingresr un número.
+
+    return number;
+}
+
+/** \brief Caclula la suma entre dos números.
+ *
+ * \param number1 float Primer operando.
+ * \param number2 float Segundo operando.
+ * \return float El resultado de la suma de ambos números.
+ *
+ */
+float add(float number1, float number2)
+{
+    float result;
+
+    result = number1 + number2; // Cálculo de la suma de ambos números recibidos como parámetros.
+
+    return result;
+}
+
+/** \brief Caclula la resta entre dos números.
+ *
+ * \param number1 float Primer operando.
+ * \param number2 float Segundo operando.
+ * \return float El resultado de la resta de ambos números.
+ *
+ */
+float subtract(float number1, float number2)
+{
+    float result;
+
+    result = number1 - number2; // Cálculo de la resta del primer número menos el segundo número recibido como parámetro.
+
+    return result;
+}
+
+/** \brief Caclula la división entre dos números.
+ *
+ * \param number1 float Primer operando.
+ * \param number2 float Segundo operando.
+ * \return float El resultado de la división entre ambos números.
+ *
+ */
+float divide(float number1, float number2)
+{
+    float result;
+
+    if(number2 == 0)
+    {
+        result = 0.00;
+    }else
+    {
+        result = number1 / number2; // Cálculo de la división entre ambos números recibidos como parámetros.
+    }
+
+    return result;
+}
+
+/** \brief Caclula la multiplicación entre dos números.
+ *
+ * \param number1 float Primer operando.
+ * \param number2 float Segundo operando.
+ * \return float El resultado de la multiplicación entre ambos números.
+ *
+ */
+float multiply(float number1, float number2)
+{
+    float result;
+
+    result = number1 * number2; // Calculo del producto de ambos números recibidos como parámetros.
+
+    return result;
+}
+
+/** \brief Caclula el factorial de un número.
+ *
+ * \param number float Operando a calcular.
+ * \return long El factorial del número.
+ *
+ */
+long int factorial(float number) //Recibe un flotante pero toma en cuenta solo la parte entera del número y calcula su factorial.
+{
+    long int result=1;
+    int i;
+
+    for(i=number; i>=1; i--) //Iteración para calcular el producto del número recibido como parámetro por cada uno de sus números naturales inferiores hasta 1 (en forma decreciente).
+    {
+        result *= i;
+    }
+
+    return result;
+}
+
+void pedirAlumno(int legajo[], int edad[], char sexo[], int nota1[], int nota2[], float promedio[], int tam)
+{
+
+    for(int i=0; i<tam; i++)
+    {
+        printf("Ingrese el legajo: ");
+        scanf("%d", &legajo[i]);
+
+        printf("Ingrese la edad: ");
+        scanf("%d", &edad[i]);
+
+        printf("Ingrese el sexo: ");
+        fflush(stdin);
+        scanf("%c", &sexo[i]);
+
+        printf("Ingrese la nota1: ");
+        scanf("%d", &nota1[i]);
+
+        printf("Ingrese la nota2: ");
+        scanf("%d", &nota2[i]);
+
+        promedio[i] = (float) (nota1[i] + nota2[i]) / NOTAS;
+    }
+}
+
+void mostrarUnAlumno(int legajo, int edad, char sexo, int nota1, int nota2, float promedio)
+{
+     printf("%d\t%d\t%c\t%d\t%d\t%.3f\n", legajo, edad, sexo, nota1, nota2, promedio);
+}
+
+void mostrarAlumnos(int legajo[], int edad[], char sexo[], int nota1[], int nota2[], float promedio[], int tam)
+{
+    printf("\nleg:\tedad:\tsexo:\tn1:\tn2:\tpromedio:\n\n");
+    for(int i=0; i<tam; i++)
+    {
+        mostrarUnAlumno(legajo[i], edad[i], sexo[i], nota1[i], nota2[i], promedio[i]);
+    }
+
+}
+
+void ordenarAlumnos(int legajo[], int edad[], char sexo[], int nota1[], int nota2[], float promedio[], int tam)
+{
+
+    int auxInt;
+    char auxChar;
+    float auxFloat;
+    int swap = 0;
+
+    for(int i=0; i<tam-1; i++)
+    {
+
+        for(int j=i+1; j<tam; j++)
+        {
+
+            if(sexo[i] > sexo[j])
+            {
+                swap = 1;
+
+            } else if (sexo[i] == sexo[j] && legajo[i] > legajo[j])
+            {
+                swap = 1;
+            }
+
+            if (swap == 1)
+            {
+
+                auxInt = legajo[i];
+                legajo[i] = legajo[j];
+                legajo[j] = auxInt;
+
+                auxInt = edad[i];
+                edad[i] = edad[j];
+                edad[j] = auxInt;
+
+                auxChar = sexo[i];
+                sexo[i] = sexo[j];
+                sexo[j] = auxChar;
+
+                auxInt = nota1[i];
+                nota1[i] = nota1[j];
+                nota1[j] = auxInt;
+
+                auxInt = nota2[i];
+                nota2[i] = nota2[j];
+                nota2[j] = auxInt;
+
+                auxFloat = promedio[i];
+                promedio[i] = promedio[j];
+                promedio[j] = auxFloat;
+
+                swap = 0;
+            }
+        }
+    }
+}
